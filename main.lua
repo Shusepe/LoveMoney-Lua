e4cbaa47d594e4999b466580585f03acf6ac3572
José Martin Altamirano <wirtex99@hotmail.com>
2022-10-22 01:41:11 -0300

    Add Movement

--- main.lua
+++ main.lua
@@ -7,6 +7,18 @@ function love.load()
 	player.h = 56
 end
 
+function love.update(dt)
+	if love.keyboard.isDown("right") then
+		player.x = player.x + 4
+	elseif love.keyboard.isDown("left") then
+		player.x = player.x - 4
+	elseif love.keyboard.isDown("down") then
+		player.y = player.y + 4
+	elseif love.keyboard.isDown("up") then
+		player.y = player.y - 4
+	end
+end
+
 function love.draw()
 	
 	-- Draw Player
