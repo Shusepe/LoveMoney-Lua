46b59bc8de6151f01b52fc88526d9cfab8c69362
José Martin Altamirano <wirtex99@hotmail.com>
2022-10-22 01:48:49 -0300

    Add Collisions

--- main.lua
+++ main.lua
@@ -1,10 +1,21 @@
+require "collision"
+
 function love.load()
+	math.randomseed(os.time())
+
 	-- Init Player
 	player = {}
 	player.x = 50
 	player.y = 300
 	player.w = 64
 	player.h = 56
+	
+	-- Init Dolar
+	dolar = {}
+	dolar.x = 150
+	dolar.y = 300
+	dolar.w = 64
+	dolar.h = 56
 end
 
 function love.update(dt)
@@ -17,10 +28,17 @@ function love.update(dt)
 	elseif love.keyboard.isDown("up") then
 		player.y = player.y - 4
 	end
+	
+	if playerDolarCollision(player.x, player.y, player.w, player.h, dolar.x, dolar.y, dolar.w, dolar.h) then
+		dolar.x = math.random(0, 800 - dolar.w)
+		dolar.y = math.random(0, 600 - dolar.h)
+	end
 end
 
 function love.draw()
 	
 	-- Draw Player
 	love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
+	
+	love.graphics.rectangle("fill", dolar.x, dolar.y, dolar.w, dolar.h)
 end
